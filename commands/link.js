54e04694c773b679c818b6d422fd84aa190f4186
Slycir <otto.hillegass@westtown.edu>
2022-01-13 16:45:41 -0500

    Link command

---
+++ commands/link.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('link')
+        .setDescription('Post hyperlink')
+        .addStringOption(option => option.setName('link').setDescription('Enter link'))
+        .addStringOption(option => option.setName('link-text').setDescription('Enter hyperlink text')),
+    async execute(interaction) {
+        const row = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                    .setLabel(interaction.options.getString('link-text'))
+                    .setStyle('LINK')
+                    .setURL(interaction.options.getString('link')),
+            );
+        return interaction.reply({components: [row]});
+    },
+};
