2cd1ef30e161e3f186294e263fc7e7cce6e70b5c
Slycir <otto.hillegass@westtown.edu>
2022-01-13 16:24:52 -0500

    Init

---
+++ commands/create.js
@@ -0,0 +1,61 @@
+const fs = require('fs')
+const { setups } = require('./setups.json')
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Permissions } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('create')
+        .setDescription('Create a new command')
+        .addStringOption(option => option.setName('command-name').setDescription('Enter command name'))
+        .addStringOption(option => option.setName('command-desc').setDescription('Enter command description'))
+        .addStringOption(option => option.setName('command-type').setDescription('Enter command type').setChoices([['Link', 'link'], ['Text', 'text']]))
+        .addStringOption(option => option.setName('command-text').setDescription('Enter command reply text'))
+        .addStringOption(option => option.setName('command-link').setDescription('If type \`link\`: Enter command reply link'))
+        .addStringOption(option => option.setName('command-link-label').setDescription('If type \`link\`: Enter command reply link label')),
+    async execute(interaction) {
+        if(!interaction.member.permissions.has(Permissions.FLAGS.MANAGE_CHANNELS)){
+            interaction.reply({ content: 'You do not have permissions', ephemeral: true })
+            return
+        }
+        var linkBool = false;
+        var commandFile;
+        const commandName = interaction.options.getString('command-name');
+        const commandDesc = interaction.options.getString('command-desc');
+        const commandText = interaction.options.getString('command-text');
+		const commandType = interaction.options.getString('command-type');
+        var commandLink;
+        var commandLinkLabel;
+		if (commandType.toLowerCase() == 'link') {
+			commandLink = interaction.options.getString('command-link');
+            commandLinkLabel = interaction.options.getString('command-link-label')
+            linkBool = true;
+        }
+        console.log(commandName)
+        if (linkBool) {
+            commandFile = `${setups[1][0]}` + `   //Command made by ${interaction.member.displayName}` + '\n'
+            for(x = 1; x < setups[1].length; x++) {
+                commandFile += `${setups[1][x]}` + '\n';
+            }
+            commandFile = commandFile.replace('${commandName}', commandName);
+            commandFile = commandFile.replace('${commandDesc}', commandDesc);
+            commandFile = commandFile.replace('${commandText}', commandText);
+            commandFile = commandFile.replace('${commandLink}', commandLink);
+            commandFile = commandFile.replace('${commandLinkLabel}', commandLinkLabel);
+        } else {
+            commandFile = `${setups[0][0]}` + `   //Command made by ${interaction.member.displayName}` + '\n'
+            for(x = 1; x < setups[0].length; x++) {
+                commandFile += `${setups[0][x]}` + '\n';
+            }
+            commandFile = commandFile.replace('${commandName}', commandName);
+            commandFile = commandFile.replace('${commandDesc}', commandDesc);
+            commandFile = commandFile.replace('${commandText}', commandText);
+        }
+        fs.writeFile(`./commands/${commandName}CR.js`, commandFile, err => {
+            if (err) {
+              console.error(err)
+            }
+        });
+        return interaction.reply('Worked');
+    },
+};
---
+++ index.js
@@ -0,0 +1,103 @@
+const fs = require('fs');
+var childProcess = require('child_process');
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+const { Client, Collection, Intents } = require('discord.js');
+const { token, conPre } = require('./config.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+runScript('./deploy-commands.js', function (err) {
+    if (err) throw err;
+});
+
+client.commands = new Collection();
+var commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+function runScript(scriptPath, callback) {
+
+    // keep track of whether callback has been invoked to prevent multiple invocations
+    var invoked = false;
+
+    var process = childProcess.fork(scriptPath);
+
+    // listen for errors as they may prevent the exit event from firing
+    process.on('error', function (err) {
+        if (invoked) return;
+        invoked = true;
+        callback(err);
+    });
+
+    // execute the callback once the process has finished running
+    process.on('exit', function (code) {
+        if (invoked) return;
+        invoked = true;
+        var err = code === 0 ? null : new Error('exit code ' + code);
+        callback(err);
+    });
+
+}
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+client.once('ready', () => {
+	console.log('Ready!');
+    sendRefresh()
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+        if(interaction.commandName == 'create') {
+            await command.execute(interaction);
+            runScript('./deploy-commands.js', function (err) {
+                if (err) throw err;
+            });
+            commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+            for (const file of commandFiles) {
+                const command = require(`./commands/${file}`);
+                client.commands.set(command.data.name, command);
+            }
+        } else {
+            await command.execute(interaction);
+        }
+	} catch (error) {
+		console.error(error);
+		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+client.login(token);
+
+function sendRefresh() {
+	readline.question('', name => {
+
+		const args = name.slice(conPre.length).trim().split(' ');
+		const command = args.shift().toLowerCase();
+		if (name.startsWith(conPre)) {
+			if (command == 'comre') {
+                runScript('./deploy-commands.js', function (err) {
+                    if (err) throw err;
+                });
+                commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+                for (const file of commandFiles) {
+                    const command = require(`./commands/${file}`);
+                    client.commands.set(command.data.name, command);
+                }
+            }
+				
+		}
+
+		sendRefresh();
+	  })
+}
