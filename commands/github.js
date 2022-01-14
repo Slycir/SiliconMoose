332d4b034af8d11bf3370ba437aeaa5909913a66
Slycir <otto.hillegass@westtown.edu>
2022-01-13 20:25:39 -0500

    Github command

---
+++ commands/github.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageActionRow, MessageButton } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('github')
+        .setDescription(''),
+    async execute(interaction) {
+        const row = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                    .setLabel('2022Robot')
+                    .setStyle('LINK')
+                    .setURL('https://github.com/MetalMooseFRC/2022Robot'),
+                new MessageButton()
+                    .setLabel('Testing-2022')
+                    .setStyle('LINK')
+                    .setURL('https://github.com/MetalMooseFRC/Testing-2022')
+            );
+        return interaction.reply({ content: 'Github links:', components: [row]});
+    },
+};
