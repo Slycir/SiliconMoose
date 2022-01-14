465579756048f99dad5970ffcee098106cce66d7
Slycir <otto.hillegass@westtown.edu>
2022-01-14 09:08:25 -0500

    Ready to go

--- commands/lastmatch.js
+++ commands/lastmatch.js
@@ -10,42 +10,55 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('lastmatch')
-        .setDescription('Last match results.'),
+        .setDescription('Last match results.')
+        .addStringOption(option => option.setName('team-number').setDescription('Look up specific team\'s last match')),
     async execute(interaction) {
         await interaction.deferReply();
         var blueTeams = ''
         var redTeams = ''
         var stor
         var color = '#4154B3'
+        var teamNumber = interaction.options.getString('team-number')
+        if(teamNumber == null) {
+            teamNumber = '1391'
+        }
         var status = await fetch(`${apiURL}/status?${auth}`).then(response => response.json());
-        var currentSeason = 2019//status.current_season
-        var Matches = await fetch(`${apiURL}/team/frc1391/matches/${currentSeason}/simple?${auth}`).then(response => response.json());
+        var currentSeason = status.current_season
+        var Matches = await fetch(`${apiURL}/team/frc${teamNumber}/matches/${currentSeason}/simple?${auth}`).then(response => response.json());
         if(Matches.length == 0){
             return interaction.editReply(`No matches played in ${currentSeason}... *yet*`);
         }
+        console.log(Matches)
         var lastMatch = Matches.reduce(function(prev, current) {
             return (prev.actual_time > current.actual_time) ? prev : current
-        }).key;
+        });
+        lastMatch = lastMatch.key;
         console.log(lastMatch)
         var Match = await fetch(`${apiURL}/match/${lastMatch}?${auth}`).then(response => response.json());
         for(x = 0; x < Match.alliances.blue.team_keys.length; x++){
             stor = Match.alliances.blue.team_keys[x].slice(3)
-            if(stor == '1391'){
+            if(stor == teamNumber){
                 stor = `**${stor}**`
             }
             blueTeams += `${stor}\n`
         }
+        for(x = 0; x < Match.score_breakdown.blue.rp; x++){
+            blueTeams += '<:rp:931542279527743558>'
+        }
         for(x = 0; x < Match.alliances.red.team_keys.length; x++){
             stor = Match.alliances.red.team_keys[x].slice(3)
-            if(stor == '1391'){
+            if(stor == teamNumber){
                 stor = `**${stor}**`
                 color = '#C80815'
             }
             redTeams += `${stor}\n`
         }
+        for(x = 0; x < Match.score_breakdown.red.rp; x++){
+            redTeams += '<:rp:931542279527743558>'
+        }
         const matchEmbed = new MessageEmbed()
 	        .setColor(color)
-	        .setTitle('1391\'s Last Match')
+	        .setTitle(`${teamNumber}\'s Last Match`)
 	        .setURL(`https://thebluealliance.com/match/${lastMatch}`)
 	        .addFields(
 		        { name: `Blue Alliance: ${Match.alliances.blue.score}`, value: `${blueTeams}`, inline: true },
