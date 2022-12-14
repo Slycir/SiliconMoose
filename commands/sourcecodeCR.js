17a25f13ecfb9cd573da72a2538b56ef0cff158f
Slycir <otto.hillegass@westtown.edu>
2022-12-14 00:12:29 -0500

    Sourcecode finder

---
+++ commands/sourcecodeCR.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');   //Command made by Otto
+const { MessageActionRow, MessageButton } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('sourcecode')
+		.setDescription('Look at the source code here'),
+	async execute(interaction) {
+		const row = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setLabel('Source Code')
+					.setStyle('LINK')
+					.setURL('https://github.com/Slycir/SiliconMoose'),
+			);
+		return interaction.reply({ content: 'Find the source code here:', components: [row]});
+	},
+};
+//This was made with the create command
