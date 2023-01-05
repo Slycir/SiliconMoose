3b5d20466df4d26ffdf32c16263e40c68f16b07e
Eupheon <ottohillegass@gmail.com>
2023-01-05 01:45:30 -0500

    2023 Setup

--- commands/vendorlibraries.js
+++ commands/vendorlibraries.js
@@ -4,27 +4,34 @@ const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('vendorlibraries')
-        .setDescription('Vendor library links.'),
+        .setDescription('Vendor library links.')
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('2023')
+                .setDescription('2023 vendor library links.')
+        ),
     async execute(interaction) {
-        const row = new ActionRowBuilder()
-            .addComponents(
-                new ButtonBuilder()
-                    .setLabel('REV Robotics')
-                    .setStyle(ButtonStyle.Link)
-                    .setURL('https://software-metadata.revrobotics.com/REVLib-2023.json'),
-                new ButtonBuilder()
-                    .setLabel('CTRE')
-                    .setStyle(ButtonStyle.Link)
-                    .setURL('https://maven.ctr-electronics.com/release/com/ctre/phoenix/Phoenix5-frc2023-latest.json'),
-                new ButtonBuilder()
-                    .setLabel('navX')
-                    .setStyle(ButtonStyle.Link)
-                    .setURL('https://dev.studica.com/releases/2023/NavX.json'),
-                new ButtonBuilder()
-                    .setLabel('PathPlannerLib')
-                    .setStyle(ButtonStyle.Link)
-                    .setURL('https://3015rangerrobotics.github.io/pathplannerlib/PathplannerLib.json')
-            );
-        return interaction.reply({ content: 'Vendor library links: (right click + copy link)', components: [row]});
+        if (interaction.options.getSubcommand() === '2023') {
+            const row = new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder()
+                        .setLabel('REV Robotics')
+                        .setStyle(ButtonStyle.Link)
+                        .setURL('https://software-metadata.revrobotics.com/REVLib-2023.json'),
+                    new ButtonBuilder()
+                        .setLabel('CTRE')
+                        .setStyle(ButtonStyle.Link)
+                        .setURL('https://maven.ctr-electronics.com/release/com/ctre/phoenix/Phoenix5-frc2023-latest.json'),
+                    new ButtonBuilder()
+                        .setLabel('navX')
+                        .setStyle(ButtonStyle.Link)
+                        .setURL('https://dev.studica.com/releases/2023/NavX.json'),
+                    new ButtonBuilder()
+                        .setLabel('PathPlannerLib')
+                        .setStyle(ButtonStyle.Link)
+                        .setURL('https://3015rangerrobotics.github.io/pathplannerlib/PathplannerLib.json')
+                );
+            return interaction.reply({ content: 'Vendor library links: (right click + copy link)', components: [row]});
+        }
     }
 };
